3db5fa8de57e06f56edaca504ca3fb3143c4cb0f
Yonatan Negash <143733511+Neptune9596@users.noreply.github.com>
2026-01-17 13:16:51 +0000

    petit changement dans l'ordre pour essayer de corriger une erreur de login

--- accueil.php
+++ accueil.php
@@ -1,7 +1,7 @@
 <?php
+    session_start();
     require "database.php";
     $pdo = Database::getConnection();
-    session_start();
     if (!isset($_SESSION["user_id"])) {
         header("Location: login.php");
     exit();
