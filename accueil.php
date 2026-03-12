4d59c023a957f55b07421e28f45a8382ecca6bd8
LE Khanh Linh <lelinhle2002@gmail.com>
2026-03-12 16:51:06 +0100

    construire l'api match

--- accueil.php
+++ accueil.php
@@ -3,10 +3,23 @@
     require "database.php";
     $pdo = Database::getConnection();
     require "Joueur.php";
-    if (!isset($_SESSION["email"])) {
-    header("Location: login.php");
-    exit();
-}   
+
+    //On garde le token dans notre session ou on redirige vers le site d'authentification
+    if (isset($_GET['token'])) {
+        $token = $_GET['token'];
+        $reponse = file_get_contents("https://authks.page.gd/verif.php?token=" . $token);
+        if ($reponse === "TRUE") {
+            $_SESSION['user_token'] = $token;
+            header("Location: accueil.php"); 
+            exit();
+        } else {
+            header("Location: https://authks.page.gd/");
+        }
+    }
+    if (!isset($_SESSION['user_token'])) {
+        header("Location: https://authks.page.gd/");
+        exit();
+    }
     
     Joueur::setPdo($pdo);
     $joueurs = Joueur::getTouslesJoueurs();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8ff1449d6b6a091abe850e3e9a711ec79b0d6f9e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8ff1449d6b6a091abe850e3e9a711ec79b0d6f9e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
