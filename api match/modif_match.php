e7fc300b8344d8bf87f10aac2dfeab3f72c06b90
lelinhle2002 <lelinhle2002@gmail.com>
2026-03-18 12:24:23 +0100

    Modifier api

--- api match/modif_match.php
+++ api match/modif_match.php
@@ -2,51 +2,11 @@
 session_start();
 require "database.php";
 require "Partie.php";
-require "api match/match.php";
+require "/Projet_PHP/api match/match.php";
 $pdo = Database::getConnection();
 if (!isset($_SESSION["email"])) {
     header("Location: login.php");
     exit();
-}
-
-if (!isset($_GET["id_match"])) {
-    exit("Erreur, ce match n'existe pas.");
-}
-$id = $_GET['id_match'];
-Partie::setPdo($pdo);
-$match = Partie::getMatchById($id);
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    //Bloquer la modification/suppression des matchs passés
-    $dateMatchComplete = new DateTime($match->getDate() . ' ' . $match->getHeure());
-    $maintenant = new DateTime();
-    
-    $data = json_decode(file_get_contents('php://input'), true);
-
-    if ($dateMatchComplete < $maintenant && $data['resultat'] === $match->getResultat()) {
-        http_response_code(403);
-        header("Location: liste_match.php");
-        echo json_encode(['message' => 'Match passé, modification non autorisée']);
-        exit();
-    }
-
-    if (isset($data['action']) && $data['action'] === 'delete') {
-        Partie::delete($id);
-        http_response_code(200);
-        echo json_encode(['message' => 'Match supprimé avec succès']);
-        exit();
-    }
-    else {
-    $match->setDateMatch($data['date_match']);
-    $match->setHeureMatch($data['heure']);
-    $match->setNomAdversaire($data['nom_adversaire']);
-    $match->setLieuDeRencontre($data['lieu_rencontre']);
-    $match->setResultat($data['resultat']);
-    http_response_code(200);
-    echo json_encode(['message' => 'Match modifié avec succès']);
-    header("Location: liste_match.php");
-    exit();
-    }
 }
 ?>
 <!DOCTYPE html>
