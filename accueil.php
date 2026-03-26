0d43d26db1140664b6e1b873c9f29e6fc13d5213
Yonatan Negash <143733511+Neptune9596@users.noreply.github.com>
2026-03-26 07:58:33 +0000

    debug encore

--- accueil.php
+++ accueil.php
@@ -24,7 +24,7 @@
              $_SESSION['user_token'] = $token;
         }else {
             unset($_SESSION['user_token']);
-            echo $token;
+            echo $resultat['status_code'];
             exit();
         }
     }
