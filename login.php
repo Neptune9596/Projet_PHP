1045a2970ff1bd056edfa481810d08d88cba75c2
Yonatan Negash <143733511+Neptune9596@users.noreply.github.com>
2026-03-26 18:44:16 +0000

    màj pour prendre en compte le role pendant la connexion

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
     session_start();
     $email = $_POST['email'] ?? "";
     $mdp   = $_POST['mdp'] ?? "";
+    $role   = $_POST['role'] ?? "";
 
     $erreurlogin = "";
 
@@ -10,7 +11,8 @@
         $url = "https://authks.page.gd/connexion.php";
         $postData = [
             'email' => $email,
-            'mdp' => $mdp
+            'mdp' => $mdp,
+            'role' => $role
         ];
 
         $ch = curl_init($url);
@@ -67,6 +69,12 @@
             <input type="text" name="email">
             <label>Mot de passe :</label>
             <input type="password" name="mdp">
+            <select name="role" id="role-select">
+                <option value="Coach">Coach</option>
+                <option value="Entraîneurs">Entraîneurs</option>
+                <option value="Directeurs">Directeurs</option>
+                <option value="Arbitres">Arbitres</option>
+            </select>
             <input type="submit" value="Se connecter">
         </form>
     </div>
